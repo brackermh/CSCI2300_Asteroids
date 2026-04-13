0188b26eef1c887f9323554310849e8b64022380
cbesancenez10 <chancebesanceenz@gmail.com>
2026-04-13 12:36:51 -0500

    Actor class 1

---
+++ src/actor.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <QGraphicsItem>
+#include <QPointF>
+#include <QPainter>
+
+class Actor : public QGraphicsItem {
+protected:
+    QPointF position;
+    float angle;
+
+public:
+    Actor();
+
+    void setRotation(float newAngle);
+    virtual void movement();
+    void collision();
+
+    QRectF boundingRect() const override;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+};
