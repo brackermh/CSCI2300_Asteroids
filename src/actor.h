fa11c0c96f986f73f6108b427dd0849bb9ae7f7d
Matt Bracker <matthew.bracker@slu.edu>
2026-04-13 13:13:28 -0500

    minor fix

--- src/actor.h
+++ src/actor.h
@@ -8,14 +8,14 @@ class Actor : public QGraphicsItem, public ObserverCollide {
 protected:
     QPointF position;
     float angle;
+    QRectF boundingRect() const override;
 
 public:
     Actor();
 
-    void setRotation(float newAngle);
+    void setRotation(int newAngle);
     virtual void movement();
     void collision();
-
-    QRectF boundingRect() const override;
+    void setBoundary(float w, float l);
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
 };
